6c6445d3fdaaa4abbc522d64d6711f394180f220
ramsathwik <ramsathwikvutukuri630@gmail.com>
2025-10-05 23:44:47 +0530

    prepare for render deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import router from "./src/routes/historicRouter.js";
 import searchRouter from "./src/routes/searchRouter.js";
 import mongoConnect from "./config/connectDb.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
 
 const app = express();
@@ -14,6 +17,16 @@ app.use(cors());
 app.use("/search", searchRouter);
 
 app.use("/analyze", router);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 const PORT = process.env.PORT;
 mongoConnect().then(() => {
