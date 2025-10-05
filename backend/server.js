2da78bc4ac23993ad547018b242a2ad1497956d5
ramsathwik <ramsathwikvutukuri630@gmail.com>
2025-10-06 00:46:03 +0530

    latest changed

--- backend/server.js
+++ backend/server.js
@@ -15,20 +15,20 @@ const app = express();
 app.use(cors());
 
 app.use("/search", searchRouter);
-
 app.use("/analyze", router);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static frontend files
-app.use(express.static(path.join(__dirname, "dist")));
+const frontendPath = path.join(__dirname, "../frontend/dist");
+app.use(express.static(frontendPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get(/^(.*)$/, (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
 mongoConnect().then(() => {
   app.listen(PORT, () => {
     logger.info(`Server is running at http://localhost:${PORT}`);
